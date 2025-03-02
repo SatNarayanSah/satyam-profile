fb1792de564c14316527245e8b9580f2542a30a4
Satyam Sah <satnarayan5166@gmail.com>
2025-03-02 20:53:06 +0545

    images added

--- app/root.tsx
+++ app/root.tsx
@@ -28,6 +28,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/satyam.jpg" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/satyam.jpg" />
         <Meta />
         <Links />
       </head>
