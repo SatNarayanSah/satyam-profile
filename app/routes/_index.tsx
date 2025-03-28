f887f1624545922f425d96d602a1f6ce402504f2
Satyam Sah <satnarayan5166@gmail.com>
2025-03-28 11:03:10 +0545

    meta description updated

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -13,7 +13,7 @@ import SkillsSection from "~/components/SkillsSection";
 export const meta: MetaFunction = () => {
   return [
     { title: "Sat Narayan Sah - MERN Stack Developer - Freelancer" },
-    { name: "description", content: "I’m Sat Narayan Sah, a MERN stack developer passionate about building scalable and dynamic web applications. I specialize in MongoDB, Express.js, React.js,  Nextjs, Remix and Node.js, creating high-performance solutions with clean code and modern web technologies. My focus is on delivering seamless user experiences and robust backend functionality." },
+    { name: "description", content: "I'm Sat Narayan Sah, a MERN stack developer crafting scalable web apps with MongoDB, Express.js, React.js, Next.js, Remix & Node.js for top performance." },
   ];
 };
 
